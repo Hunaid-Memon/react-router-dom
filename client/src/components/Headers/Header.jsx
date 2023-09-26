68ab12d73e6c6e3a384fe2048f6cb606875bee0e
Hunaid <hunaid.hanif@gmail.com>
2023-09-26 12:29:49 +0000

    install react-router-dom

---
+++ client/src/components/Headers/Header.jsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+const Headers = () => {
+    return (
+        <div>
+            Headers
+        </div>
+    )
+}
+
+export default Headers;
